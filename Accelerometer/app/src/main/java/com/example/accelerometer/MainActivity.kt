a1daa9fcf665a9737f0dafcafe697a21838ea49f
Marcin Staniek <marcin.staniek97@gmail.com>
2020-04-27 12:14:03 +0200

    X, Y, Z Accelerometer Data

--- Accelerometer/app/src/main/java/com/example/accelerometer/MainActivity.kt
+++ Accelerometer/app/src/main/java/com/example/accelerometer/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.accelerometer
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensorManager.registerListener(
+            this,
+            sensorManager.getDefaultSensor((Sensor.TYPE_ACCELEROMETER)),
+            SensorManager.SENSOR_DELAY_NORMAL
+        )
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        accelerometer_data.text = "x = ${event!!.values[0]}\n\n" +
+                "y = ${event.values[1]}" +
+                "z = ${event.values[2]}"
     }
 }
